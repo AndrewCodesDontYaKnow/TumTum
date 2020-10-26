60c81c475fb6404d51f2f91f6d23cca13f7e77ab
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-10-26 13:17:52 -0500

    Updates

---
+++ precache-manifest.6a57b2af8c375a8a94d141d14bdf5351.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ded29fbd2aa887017071affed8c847",
+    "url": "/tumtum/index.html"
+  },
+  {
+    "revision": "39231c42e51d05f90512",
+    "url": "/tumtum/static/css/main.dadbaf83.chunk.css"
+  },
+  {
+    "revision": "5d8cf13ce1199e8698a6",
+    "url": "/tumtum/static/js/2.cf689eec.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/tumtum/static/js/2.cf689eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39231c42e51d05f90512",
+    "url": "/tumtum/static/js/main.767d89f8.chunk.js"
+  },
+  {
+    "revision": "b8029bb9ae4b813270a7",
+    "url": "/tumtum/static/js/runtime-main.3630c1c0.js"
+  },
+  {
+    "revision": "e266b7d33f08d2a81b69b51da89fa30b",
+    "url": "/tumtum/static/media/background_search_desktop.e266b7d3.jpg"
+  },
+  {
+    "revision": "ba0e5eec14129567a6e56168ec6fee97",
+    "url": "/tumtum/static/media/background_search_mobile.ba0e5eec.jpg"
+  }
+]);
