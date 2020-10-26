c0d45ef827f3dfb904c902827835bd8420eb9c14
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-10-26 12:30:10 -0500

    fixed yelp api call

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,11 +1,27 @@
 // Client ID
 // w5tIE6SLQwThhuOXL9eXoQ
+import queryString from 'query-string';
 
 const apiKey =
   "krSGtOieQbHcaagQMTjsSyRwWzFw-AuHw7NyR40w1n9nhakj-jxv88KxJQAEWJYUc0fdHiMk9F80Ng-ZMuxQyYQRlBA7i-xEo8U_n-W3_s6WgsKh8ucMCDrIHROTX3Yx";
 
 const Yelp = {
     
+
+
+    // search(path, queryParams) {
+    //     const query = queryString.stringify(queryParams);
+
+    //     return fetch(`https://api.yelp.com/v3${path}?${query}`, {
+    //         headers: {
+    //             Authorization: `Bearer ${apiKey}`,
+    //             Origin: 'localhost',
+    //             withCredentials: true,
+    //         }
+    //     })
+    // }
+
+
   search(term, location, sortBy) {
     return fetch(
       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}
